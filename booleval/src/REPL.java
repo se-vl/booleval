bdd2697e86d73843cd9fb65617b90ec605e8d871
se-vl <se-vl@users.noreply.github.com>
2015-04-14 11:11:17 +0200

    add read-eval-print-loop and main method
    Now the user can enter formulae at runtime :)

---
+++ booleval/src/REPL.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class REPL
+{
+    private final Scanner _scanner;
+    private String _line;
+    private final Parser _parser;
+
+    public REPL()
+    {
+        _scanner = new Scanner(System.in);
+        _parser = new Parser();
+    }
+
+    public void start()
+    {
+        printWelcomeMessage();
+        readLine();
+        while (!_line.isEmpty())
+        {
+            processLine();
+            System.out.println();
+            readLine();
+        }
+    }
+
+    private void printWelcomeMessage()
+    {
+        System.out.println("Enter boolean postfix formulae containing the variables a and b.");
+        System.out.println();
+        System.out.println("Just pressing enter exits the program. Have fun!");
+        System.out.println();
+    }
+
+    private void readLine()
+    {
+        System.out.print("$ ");
+        _line = _scanner.nextLine();
+    }
+
+    private void processLine()
+    {
+        Expression expression = _parser.parse(_line);
+        System.out.println(expression);
+    }
+}
